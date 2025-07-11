b28c16c66d98f4542cddcd300c91999641c418e8
BIGBOSS019 <camil.aliyev838@gmail.com>
2025-07-11 14:15:23 +0400

    Клфссная работа

---
+++ JavaforJamil9/src/AtClass/Class7.java
@@ -0,0 +1,17 @@
+package AtClass;
+
+public class Class7 {
+    public static void main(String[] args) {
+        String str1 = "Java is a programming language";
+        String str2 = "Java is amazing";
+
+        System.out.println(str1.length()); //длина
+        System.out.println(str1.toLowerCase()); // прописные abcd
+        System.out.println("----------------");
+        System.out.println(str2.length()); //длина
+        System.out.println(str2.toUpperCase()); // заглавные ABCD
+        System.out.println("----------------");
+        System.out.println(str1 == str2);
+
+    }
+}
